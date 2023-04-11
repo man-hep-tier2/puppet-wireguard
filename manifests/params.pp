11d4bd928eff620c8969048b298f713bc538be47
Robert Frank <robert.frank@manchester.ac.uk>
2023-04-11 11:03:52 +0200

    Fix string.

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,6 +14,7 @@ class wireguard::params {
     'Rocky': {
       $manage_repo    = false
       $package_name   = ['wireguard-tools']
+      $repo_url       = ''
     }
     'Ubuntu': {
       $manage_repo    = false
